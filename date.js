bcfebb7a5cb50224d522ceb09f132e3733d6720f
おかーかじぇーぴー <kaitosatou0025@gmail.com>
2024-01-10 16:40:22 +0900

    Create date.js

---
+++ date.js
@@ -0,0 +1,7 @@
+const date = new Date();
+const datey = date.getFullYear();
+const dated = date.getDate();
+const datem = date.getMonth();
+const dateh = date.getHours();
+const datemi = date.getMinutes();
+document.write(datey + dated + datem + datemi)
